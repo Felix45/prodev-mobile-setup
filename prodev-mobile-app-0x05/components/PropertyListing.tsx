0d5d969238190d59948759c3f552ca8a3ea07cdb
felix45 <fatonoh@gmail.com>
2025-07-20 15:42:42 +0300

    Remove uncommented code

--- prodev-mobile-app-0x05/components/PropertyListing.tsx
+++ prodev-mobile-app-0x05/components/PropertyListing.tsx
@@ -1,7 +1,5 @@
 import { View, Text } from "react-native";
 import PropertyListingCard from "./common/PropertyListingCard";
-// import { useState } from "react";
-// Update the import path if the file is named 'interfaces.ts' or 'interfaces/index.ts'
 import { PropertListing, PropertyListingProps } from "../interfaces/index";
 
 const PropertyListing = ({ listings }: PropertListing) => {
